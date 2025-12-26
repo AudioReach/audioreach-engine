1284a433e6e617bcedeb8c2643bd11be2bcfe839
hahire <hahire@qti.qualcomm.com>
2025-12-26 16:49:48 +0530

    audioreach-engine: fix compilation issue.
    Config file was getting copied after
    CONFIG_PROC_DOMAIN was updated, which resulted
    in NULL value CONFIG_PROC_DOMAIN.
    
    Signed-off-by: ffrancis <ffrancis@qti.qualcomm.com>

--- Android.mk
+++ Android.mk
@@ -5,6 +5,7 @@ PROJECT_BINARY_DIR := $(MY_LOCAL_PATH_ARE)/build/linux/
 PROJECT_SOURCE_DIR := $(MY_LOCAL_PATH_ARE)
 GLOBAL_SPF_LIBS_LIST :=
 
+include $(MY_LOCAL_PATH_ARE)/scripts/make/kconfig_parser.mk
 # read CONFIG_PROC_DOMAIN from .config file
 CONFIG_LINE := $(file < $(PROJECT_BINARY_DIR).config)
 CONFIG_PROC_DOMAIN := $(patsubst CONFIG_PROC_DOMAIN=%,%,$(filter CONFIG_PROC_DOMAIN=%,$(CONFIG_LINE)))
@@ -27,6 +28,5 @@ endif
 # Set derived variable
 PROC_DOMAIN_NAME := PROC_DOMAIN_$(CONFIG_PROC_DOMAIN)
 
-include $(MY_LOCAL_PATH_ARE)/scripts/make/kconfig_parser.mk
 include $(MY_LOCAL_PATH_ARE)/modules/build/Android.mk
 include $(MY_LOCAL_PATH_ARE)/fwk/build/Android.mk
