a872fa75f794a5dfe2a549ab2a3804efe8d622ec
ffrancis <ffrancis@qti.qualcomm.com>
2026-03-11 12:59:13 +0530

    audioreach-engine: fix modules XML generation
    Modules were getting compiled before proc_domain
    was set, so change the sequence for compilation.
    
    Signed-off-by: ffrancis <ffrancis@qti.qualcomm.com>

--- Android.mk
+++ Android.mk
@@ -4,9 +4,6 @@ BUILD_ARE_MODULES := $(MY_LOCAL_PATH_ARE)/scripts/make/spf_base.mk
 PROJECT_BINARY_DIR := $(MY_LOCAL_PATH_ARE)/build/linux/
 PROJECT_SOURCE_DIR := $(MY_LOCAL_PATH_ARE)
 GLOBAL_SPF_LIBS_LIST :=
-include $(MY_LOCAL_PATH_ARE)/scripts/make/kconfig_parser.mk
-include $(MY_LOCAL_PATH_ARE)/modules/build/Android.mk
-include $(MY_LOCAL_PATH_ARE)/fwk/build/Android.mk
 
 # read CONFIG_PROC_DOMAIN from .config file
 CONFIG_LINE := $(file < $(PROJECT_BINARY_DIR).config)
@@ -29,3 +26,7 @@ endif
 
 # Set derived variable
 PROC_DOMAIN_NAME := PROC_DOMAIN_$(CONFIG_PROC_DOMAIN)
+
+include $(MY_LOCAL_PATH_ARE)/scripts/make/kconfig_parser.mk
+include $(MY_LOCAL_PATH_ARE)/modules/build/Android.mk
+include $(MY_LOCAL_PATH_ARE)/fwk/build/Android.mk
