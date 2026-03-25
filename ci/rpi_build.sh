36101d4ec901e210280392d9e3cf5b7e409608c8
ffrancis123 <ffrancis@qti.qualcomm.com>
2026-03-25 18:29:19 +0530

    Update rpi_build.sh
    Signed-off-by: ffrancis123 <ffrancis@qti.qualcomm.com>

--- ci/rpi_build.sh
+++ ci/rpi_build.sh
@@ -5,6 +5,7 @@
 set -ex
 
 
+
 # Source SDK environment 
 ## <strong>ToDo</strong>: Need to update for SDK path and GITHUB_WORKSPACE handling.
 source ${GITHUB_WORKSPACE}/install/environment-setup-cortexa7t2hf-neon-vfpv4-poky-linux-gnueabi
