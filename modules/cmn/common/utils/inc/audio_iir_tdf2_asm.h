4c099d3ade0536a3e2df7adb8539dd5cbbc87ffa
Annemarie Porter <annemari@qti.qualcomm.com>
2026-06-04 10:11:49 -0700

    Fix Hexagon build errors in iir_tdf2 and audio_filter
      Two build failures observed when compiling for Hexagon targets:
    
      1. iir_tdf2.c includes audio_iir_tdf2_asm.h under __hexagon__
         guard but the header was absent from the repository:
    
           iir_tdf2.c:14:10:
             fatal error: 'audio_iir_tdf2_asm.h' file not found
    
      2. biquad_process_io() in audio_filter.cpp accesses yL16 and
         xL16 members absent from the Hexagon _biquadFilter variant:
    
           audio_filter.cpp:465:27:
             error: no member named 'yL16' in '_biquadFilter'
           audio_filter.cpp:466:27:
             error: no member named 'xL16' in '_biquadFilter'
    
      Add audio_iir_tdf2_asm.h to modules/cmn/common/utils/inc/ and
      fix the missing Hexagon guard in biquad_process_io().
    
    Signed-off-by: Annemarie Porter <annemari@qti.qualcomm.com>

---
+++ modules/cmn/common/utils/inc/audio_iir_tdf2_asm.h
@@ -0,0 +1,19 @@
+/*========================================================================*/
+/**
+@file audio_iir_tdf2_asm.h
+
+This file defines flags to use Q6 ASM of IIR TDF2 functions.
+*/
+
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef _AUDIO_IIRTDF2_ASM_H_
+#define _AUDIO_IIRTDF2_ASM_H_
+
+#define    QDSP6_ASM_IIRTDF2_32
+#define    QDSP6_ASM_IIRTDF2_16
+
+#endif  /* _AUDIO_IIRTDF2_ASM_H_ */
